78d1af369cef6a3712575113bbce13eb8eb8b4fa
Fabián Villena <villenafabian@gmail.com>
2021-01-26 21:29:48 -0300

    initial commit

---
+++ index.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import whoosh.fields
+import whoosh.index
+import whoosh.qparser
+import whoosh.analysis
+import whoosh.support.charset
+import whoosh.scoring
+import os
+import csv
+
+
+my_analyzer = whoosh.analysis.LanguageAnalyzer("es") | whoosh.analysis.CharsetFilter(whoosh.support.charset.accent_map)# | whoosh.analysis.NgramFilter(minsize=2, maxsize=4)
+
+
+
+schema = whoosh.fields.Schema(
+    code = whoosh.fields.ID(stored=True),
+    description = whoosh.fields.TEXT(stored=True,analyzer=my_analyzer)
+)
+
+
+if not os.path.exists("index"):
+    os.mkdir("index")
+ix = whoosh.index.create_in("index", schema)
+
+writer = ix.writer()
+
+
+with open('cie-o.csv', newline='', encoding="utf-8-sig") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=";")
+    for row in reader:
+        writer.add_document(code=row["code"],description=row["description"])
+
+
+writer.commit()
---
+++ main.py
@@ -0,0 +1,22 @@
+import fastapi
+import whoosh.index
+import whoosh.qparser
+
+app = fastapi.FastAPI(title="CIE-O-3-M Search Engine", description="API for searching in CIE-O-3-M", version="1.0")
+
+ix = whoosh.index.open_dir("index")
+parser = whoosh.qparser.QueryParser("description", ix.schema, group=whoosh.qparser.OrGroup.factory(0.9))
+searcher = ix.searcher()
+
+@app.get("/cie-o-m")
+async def search_cieom(q: str):  
+    myquery = parser.parse(q)
+    results = searcher.search(myquery, limit=10, terms=True)
+    response = []
+    for result in results:
+        response.append(result.fields())
+    return response
+
+@app.on_event('shutdown')
+def shutdown():
+    searcher.close()
